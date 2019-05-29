aa55947400ace67886fed74fa36dcf7d89f2b048
Andrew Kane <andrew@chartkick.com>
2019-05-28 21:02:28 -0700

    Bye jquery

--- lib/notable_web/engine.rb
+++ lib/notable_web/engine.rb
@@ -2,7 +2,10 @@ module NotableWeb
   class Engine < ::Rails::Engine
     isolate_namespace NotableWeb
 
-    initializer "notable_web" do
+    initializer "notable_web" do |app|
+      # use a proc instead of a string
+      app.config.assets.precompile << proc { |path| path == "notable_web.js" }
+
       NotableWeb.time_zone ||= Time.zone
     end
   end
